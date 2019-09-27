08f681d75ce6aef7fa7309251f7abfd647d55b09
net-ka <a.k.fedosova@gmail.com>
2019-09-28 01:47:35 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -38,7 +38,55 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let singleArr = expr.split('');
+    let tenArr = [];
+
+    for (i = 10; i <= singleArr.length; i += 10 ) {
+        tenArr.push(singleArr.slice((i - 10), i));
+    }
+
+    let tenTwiceArr = [];
+
+    for (i = 0; i < tenArr.length; i ++) {
+        let bufferTwiceArr = [];
+        for ( k = 0; k < tenArr[i].length; k += 2 ) {
+            bufferTwiceArr.push(tenArr[i][k] + '' + tenArr[i][k + 1]);
+        }
+        tenTwiceArr.push(bufferTwiceArr);
+    }
+
+    for (i = 0; i < tenTwiceArr.length; i ++) {
+        for ( k = 0; k < tenTwiceArr[i].length; k ++ ) {
+            if (tenTwiceArr[i][k] === '00') {
+                tenTwiceArr[i][k] = '';
+            }
+            if (tenTwiceArr[i][k] === '10') {
+                tenTwiceArr[i][k] = '.';
+            }
+            if (tenTwiceArr[i][k] === '11') {
+                tenTwiceArr[i][k] = '-';
+            }
+        }
+    }
+
+    let signsArr = [];
+    for (i = 0; i < tenTwiceArr.length; i ++) {
+        if (tenTwiceArr[i] === '') {
+            tenTwiceArr[i] = ' ';
+        }
+        signsArr.push(tenTwiceArr[i].join(''));
+    }
+
+    for (i = 0; i < signsArr.length; i ++) {
+        if (signsArr[i] in MORSE_TABLE) {
+            let a = signsArr[i];
+            signsArr[i] = MORSE_TABLE[a];
+        } else {
+            signsArr[i] = ' ';
+        }
+    }
+
+    return signsArr.join('');
 }
 
 module.exports = {
